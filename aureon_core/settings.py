591dee718266791d8be79441f1d0965757ec46a0
Jefferson Fernandes <suportefinanceiro@rcostaadv.com.br>
2025-08-24 09:01:58 -0300

    fix: Simplifica e corrige definitivamente ALLOWED_HOSTS

--- aureon_core/settings.py
+++ aureon_core/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,14 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7#*x0#2pc9zd6gc^=l)#+bm+vgt(8*p$l&ovhl5ujmg_m5zj=-'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-7#*x0#2pc9zd6gc^=l)#+bm+vgt(8*p$l&ovhl5ujmg_m5zj=-')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'aureon-g886.onrender.com',
+    '127.0.0.1',
+]
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -73,11 +77,17 @@ WSGI_APPLICATION = 'aureon_core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+# Se não estiver no Render, use o banco de dados local sqlite3 para desenvolvimento
+if 'RENDER' not in os.environ:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
 
 # Password validation
